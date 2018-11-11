577a8be0560ce86571f83e96736984dfc9424012
Roberto <robertoeat91@gmail.com>
2018-11-10 21:46:23 -0400

    All project files uploaded by first time

---
+++ app/js/scripts.js
@@ -0,0 +1,14 @@
+jQuery( document ).ready( function( $ ) {
+
+  'use strict';
+
+  $( '.carousel-testimonies').slick({
+
+      dots: false,
+      arrows: true,
+        slidesToShow: 1,
+        draggable: false
+
+    });
+
+} );
