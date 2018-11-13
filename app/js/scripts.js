257cc5fe0ba5caec7831359999c52cfd25787e77
Roberto <robertoeat91@gmail.com>
2018-11-12 23:02:52 -0400

    Design process changes

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,13 +2,13 @@ jQuery( document ).ready( function( $ ) {
 
   'use strict';
 
-  $( '.carousel-testimonies').slick({
-
-      dots: false,
-      arrows: true,
-        slidesToShow: 1,
-        draggable: false
-
-    });
+  // $( '.carousel-testimonies').slick({
+  //
+  //     dots: false,
+  //     arrows: true,
+  //       slidesToShow: 1,
+  //       draggable: false
+  //
+  //   });
 
 } );
