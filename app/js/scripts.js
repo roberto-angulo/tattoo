1d8bf0f091bcf7e13e1f4f18f2163a85db93274a
Roberto <robertoeat91@gmail.com>
2018-11-15 18:13:29 -0400

    Slick carousel plugin removed, and Magnific Popup added

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -8,4 +8,19 @@ jQuery( document ).ready( function( $ ) {
 
     } );
 
+    // Gallery popup
+
+    $('.project-item').magnificPopup({
+   type: 'image',
+     // Delay in milliseconds before popup is removed
+    removalDelay: 300,
+
+    // Class that is added to popup wrapper and background
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade',
+     gallery:{
+       enabled: true
+     }
+  });
+
 } );
